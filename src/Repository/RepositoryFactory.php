a1a265a7b769023bee714ce5bd93ce87de3e4a13
mohamed00Sayed <sayedm22529@gmail.com>
2023-02-25 16:36:55 +0200

    add repository factory; a central place for repositories

---
+++ src/Repository/RepositoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Moham\Test\Repository;
+
+class RepositoryFactory
+{
+    private $REPOS;
+    public const BOOK = 'book';
+    public const DVD = 'dvd';
+    public const FURNITURE = 'furniture';
+
+    public function __construct()
+    {
+        $this->REPOS = [
+            RepositoryFactory::BOOK => new BookRepository(),
+            RepositoryFactory::DVD => new DvdRepository(),
+            RepositoryFactory::FURNITURE => new FurnitureRepository()
+        ];
+    }
+
+    public  function getRepository(string $type): Repository
+    {
+        return $this->REPOS[$type];
+    }
+}
