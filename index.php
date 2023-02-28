0ed6deed46a35d575e84739d590442477c2cc222
mohamed00Sayed <sayedm22529@gmail.com>
2023-02-28 16:47:54 +0200

    add request handlers functionality.

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 use Dotenv\Dotenv;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Middlewares\Cors;
+use Moham\Test\Builder\ProductBuilderFactory;
+use Moham\Test\Repository\RepositoryFactory;
 use Moham\Test\Servlet\HttpServlet;
 use Moham\Test\Util\ParsersContainer;
 use Moham\Test\Util\ResponseFactory as UtilResponseFactory;
@@ -15,17 +17,18 @@ use Relay\RelayBuilder;
 use Yiisoft\Request\Body\RequestBodyParser;
 
 require 'vendor/autoload.php';
-/*CORS settings*/
-$settings = (new Settings())->init("http", "localhost", 8000);
-$settings->setAllowedOrigins(["*"]);
-$settings->setAllowedHeaders(["Content-Type"]);
-$settings->setAllowedMethods(["GET", "POST", "DELETE"]);
-$analyzer = Analyzer::instance($settings);
 
 /*Read environment variables from .env file*/
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
+/*CORS settings*/
+$settings = (new Settings())->init($_ENV["HOST_PROTOCOL"], $_ENV["HOST_IP"], $_ENV["HOST_PORT"]);
+$settings->setAllowedOrigins((array)$_ENV["ALLOWED_ORIGINS"]);
+$settings->setAllowedHeaders((array)$_ENV["ALLOWED_HEADERS"]);
+$settings->setAllowedMethods((array)$_ENV["ALLOWED_METHODS"]);
+$analyzer = Analyzer::instance($settings);
+$cors = new Cors($analyzer);
 /*Create request body parser*/
 $requestBodyParser =  new RequestBodyParser(new UtilResponseFactory(), new ParsersContainer);
 
@@ -33,21 +36,71 @@ $requestBodyParser =  new RequestBodyParser(new UtilResponseFactory(), new Parse
 $dispatcherServlet = new HttpServlet();
 
 $dispatcherServlet->get("/products/", function ($request) {
-    return createResponse(200, "It's Ok");
+    $repoFactory = new RepositoryFactory();
+    $booksRepo = $repoFactory->getRepository(RepositoryFactory::BOOK);
+    $dvdsRepo = $repoFactory->getRepository(RepositoryFactory::DVD);
+    $furnituresRepo = $repoFactory->getRepository(RepositoryFactory::FURNITURE);
+    $resData = [
+        'books' => $booksRepo->getAll(),
+        'dvds' => $dvdsRepo->getAll(),
+        'furnitures' => $furnituresRepo->getAll()
+    ];
+    return createResponse(200, json_encode($resData));
 });
 
 $dispatcherServlet->post("/products/", function ($request) {
-    return createResponse(200, json_encode($request->getParsedBody() . " OK ^_^"));
+    $builderFactory = new ProductBuilderFactory();
+    $repoFactory = new RepositoryFactory();
+
+    $parsedBody = convert($request->getParsedBody());
+
+    $booksRepo = $repoFactory->getRepository(RepositoryFactory::BOOK);
+    $dvdsRepo = $repoFactory->getRepository(RepositoryFactory::DVD);
+    $furnituresRepo = $repoFactory->getRepository(RepositoryFactory::FURNITURE);
+
+    $saveRepo = $repoFactory->getRepository($parsedBody->type);
+
+    $builder = $builderFactory->getBuilder($parsedBody->type);
+    $product = $builder->getProductInstance($parsedBody->data);
+    /*check existence of the product in all tables*/
+    $sku_in_books = $booksRepo->exists($product->getSku());
+    $sku_in_dvds = $dvdsRepo->exists($product->getSku());
+    $sku_in_furnitures = $furnituresRepo->exists($product->getSku());
+
+    $res = ['error' => 'Already exists'];
+    $code = 403;
+    if (!$sku_in_books && !$sku_in_dvds && !$sku_in_furnitures) {
+        $res = $saveRepo->save($product);
+        if($res){
+            $code = 200;
+        }
+    }
+    return createResponse($code, json_encode($res));
 });
 
 $dispatcherServlet->delete("/products/", function ($request) {
-    return createResponse(200, "Yes indeedeo !!! chandler bing");
+    $repoFactory = new RepositoryFactory();
+    $booksRepo = $repoFactory->getRepository(RepositoryFactory::BOOK);
+    $dvdsRepo = $repoFactory->getRepository(RepositoryFactory::DVD);
+    $furnituresRepo = $repoFactory->getRepository(RepositoryFactory::FURNITURE);
+
+    $parsedBody = $request->getParsedBody();
+
+    $books_skus = $parsedBody["books"];
+    $dvds_skus = $parsedBody["dvds"];
+    $furnitures_skus = $parsedBody["furnitures"];
+    /*No side effects from deleting a non existing sku*/
+    $booksRepo->deleteAll($books_skus);
+    $dvdsRepo->deleteAll($dvds_skus);
+    $furnituresRepo->deleteAll($furnitures_skus);
+    /*So, alaway return OK*/
+    return createResponse(200, "OK");
 });
 
 /*Build Relay*/
 $builder = new RelayBuilder();
 $relay = $builder->newInstance([
-    new Cors($analyzer),
+    $cors,
     $requestBodyParser,
     $dispatcherServlet
 ]);
@@ -76,10 +129,22 @@ function createRequest()
     return $request;
 }
 /*A function to create response*/
-function createResponse(int $status, string $data): ResponseInterface
+function createResponse(int $status = 200, string $data): ResponseInterface
 {
     $psr17Factory = new Psr17Factory();
     $responseBody = $psr17Factory->createStream($data);
     $response = $psr17Factory->createResponse($status)->withBody($responseBody);
     return $response;
+}
+/*A function to convert an array to stdClass instance*/
+function convert(array $array): stdClass
+{
+    $instance = new stdClass();
+    foreach ($array as $k => $v) {
+        if (is_array($v)) {
+            $v = convert($v);
+        }
+        $instance->$k = $v;
+    }
+    return $instance;
 }
